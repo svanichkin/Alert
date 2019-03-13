6ac5eccabbbf2295266810475c80f457689aeb6d
Sergey <sergey@vanichkin.ru>
2019-03-13 08:45:58 +0300

    Add files via upload

---
+++ Alert.h
@@ -0,0 +1,58 @@
+//
+//  Alert.h
+//  v.1.3
+//
+//  Created by Сергей Ваничкин on 3/12/19.
+//  Copyright © 2019 Сергей Ваничкин. All rights reserved.
+//
+//  Пример использования
+//
+//  [Alert showWithTitle:@"Title"
+//               message:@"Text"
+//               buttons:@[[@"Destructive" destructiveStyle],
+//                         @"OK",
+//                         [@"Cancel" cancelStyle]]
+//               handler:^(NSInteger buttonIndex)
+//   {
+//       if (buttonIndex == 0)
+//       {
+//       }
+//   }];
+
+#import <Foundation/Foundation.h>
+#import "TargetConditionals.h"
+
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#else
+#import <Cocoa/Cocoa.h>
+#endif
+
+#define OK_BUTTON @"OK"
+
+@interface DefaultButtonStyle : NSString
+@end
+
+@interface CancelButtonStyle : DefaultButtonStyle
+@end
+
+@interface DestructiveButtonStyle : DefaultButtonStyle
+@end
+
+@interface NSString (AlertButtonStyle)
+
+-(CancelButtonStyle      *)cancelStyle;
+-(DestructiveButtonStyle *)destructiveStyle;
+
+@end
+
+typedef void(^AlertButtonHandle)(NSInteger buttonIndex);
+
+@interface Alert : NSObject
+
++(void)showWithTitle:(NSString             *)title
+             message:(NSString             *)message
+             buttons:(NSArray <NSString *> *)buttons
+             handler:(AlertButtonHandle     )handler;
+
+@end
---
+++ Alert.m
@@ -0,0 +1,202 @@
+//
+//  Alert.m
+//  v.1.3
+//
+//  Created by Сергей Ваничкин on 3/12/19.
+//  Copyright © 2019 Сергей Ваничкин. All rights reserved.
+//
+
+#import "Alert.h"
+
+@interface DefaultButtonStyle ()
+
+@property (nonatomic, strong) NSString *stringHolder;
+
+@end
+
+@implementation DefaultButtonStyle
+
+-(instancetype)initWithCharactersNoCopy:(unichar  *)characters
+                                 length:(NSUInteger)length
+                           freeWhenDone:(BOOL      )freeBuffer
+{
+    if (self = [super init])
+    {
+        self.stringHolder =
+        [NSString.alloc
+         initWithCharactersNoCopy:characters
+         length:length
+         freeWhenDone:freeBuffer];
+    }
+    return self;
+}
+
+-(NSUInteger)length
+{
+    return
+    self.stringHolder.length;
+}
+
+-(unichar)characterAtIndex:(NSUInteger)index
+{
+    return
+    [self.stringHolder characterAtIndex:index];
+}
+
+@end
+
+@implementation CancelButtonStyle
+
+-(instancetype)initWithCharactersNoCopy:(unichar  *)characters
+                                 length:(NSUInteger)length
+                           freeWhenDone:(BOOL      )freeBuffer
+{
+    self = [super initWithCharactersNoCopy:characters
+                                    length:length
+                              freeWhenDone:freeBuffer];
+
+    return self;
+}
+
+@end
+
+@implementation DestructiveButtonStyle
+
+-(instancetype)initWithCharactersNoCopy:(unichar  *)characters
+                                 length:(NSUInteger)length
+                           freeWhenDone:(BOOL      )freeBuffer
+{
+    self =
+    [super initWithCharactersNoCopy:characters
+                             length:length
+                       freeWhenDone:freeBuffer];
+
+    return self;
+}
+
+@end
+
+@implementation NSString (AlertButtonStyle)
+
+-(CancelButtonStyle      *)cancelStyle
+{
+    return
+    [CancelButtonStyle.alloc initWithString:self];
+}
+
+-(DestructiveButtonStyle *)destructiveStyle
+{
+    return
+    [DestructiveButtonStyle.alloc initWithString:self];
+}
+
+@end
+
+@implementation Alert
+
+#if TARGET_OS_IPHONE
++(void)showWithTitle:(NSString             *)title
+             message:(NSString             *)message
+             buttons:(NSArray <NSString *> *)buttons
+             handler:(AlertButtonHandle     )handler
+{
+    UIAlertController *controller =
+    [UIAlertController
+     alertControllerWithTitle:title
+     message:message
+     preferredStyle:UIAlertControllerStyleAlert];
+    
+    NSMutableArray *array =
+    NSMutableArray.new;
+    
+    if (buttons == nil)
+        [array addObject:OK_BUTTON];
+    
+    else
+        [array addObjectsFromArray:buttons];
+    
+    for (id button in array)
+    {
+        UIAlertActionStyle style =
+        UIAlertActionStyleDefault;
+        
+        if ([button isKindOfClass:CancelButtonStyle.class])
+            style =
+            UIAlertActionStyleCancel;
+        
+        if ([button isKindOfClass:DestructiveButtonStyle.class])
+            style =
+            UIAlertActionStyleDestructive;
+        
+        [controller addAction:
+         [UIAlertAction
+          actionWithTitle:button
+          style:style
+          handler:^(UIAlertAction *action)
+          {
+              handler([array indexOfObject:button]);
+          }]];
+    }
+    
+    dispatch_async(dispatch_get_main_queue(), ^(void)
+    {
+        UIWindow *window =
+        UIWindow.new;
+        
+        window.backgroundColor =
+        UIColor.clearColor;
+        
+        [window makeKeyAndVisible];
+        
+        NSInteger maxZOrder = NSIntegerMin;
+        
+        for (UIWindow *w in UIApplication.sharedApplication.windows)
+            if (w.windowLevel > maxZOrder && w != window)
+                maxZOrder = window.windowLevel;
+        
+        window.windowLevel = maxZOrder + 1;
+        
+        window.rootViewController =
+        UIViewController.new;
+        
+        [window.rootViewController
+         presentViewController:controller
+         animated:YES
+         completion:nil];
+    });
+}
+
+#else
++(void)showWithTitle:(NSString             *)title
+             message:(NSString             *)message
+             buttons:(NSArray <NSString *> *)buttons
+             handler:(AlertButtonHandle     )handler
+{
+    NSAlert *alert =
+    [NSAlert alertWithError:self];
+    
+    alert.messageText = title;
+    
+    if (buttons == nil)
+        [alert addButtonWithTitle:OK_BUTTON];
+    
+    for (AlertButton *button in buttons)
+        [alert addButtonWithTitle:button.title];
+    
+    for (NSButton *button in alert.buttons)
+        button.tag =
+        [alert.buttons indexOfObject:button];
+    
+    dispatch_async(dispatch_get_main_queue(), ^(void)
+    {
+        [alert beginSheetModalForWindow:NSApp.mainWindow
+                      completionHandler:^(NSModalResponse returnCode)
+        {
+            handler([alert.buttons[returnCode] tag]);
+        }];
+    });
+}
+
+#endif
+
+@end
